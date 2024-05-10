551afd0d40f1ab2923b746137949865fb6a2b99d
Flarp <flurpdadurp@gmail.com>
2024-05-09 20:44:10 -0400

    Use Diesel errors to put 404

--- src/api/error.rs
+++ src/api/error.rs
@@ -9,12 +9,18 @@ pub enum DeductError {
     UnauthorizedUser(String),
     
     #[response(status = 400)]
-    BadRequest(String)
+    BadRequest(String),
+
+    #[response(status = 404)]
+    NotFound(String)
 }
 
 impl From<diesel::result::Error> for DeductError {
     fn from(err: diesel::result::Error) -> DeductError {
-        Self::DieselError(Debug(err))
+        match err {
+            diesel::result::Error::NotFound => Self::NotFound("Not found".to_string()),
+            _ => Self::DieselError(Debug(err))
+        }
     }
 }
 
